8f14fb4fac80f3340abe698a96f86705f8aeb2f0
Karan Thakker <karan17thakkar@gmail.com>
2024-07-31 17:23:19 +0800

    Created User Service

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.example.demo.controller;
+
+import com.example.demo.entity.User;
+import com.example.demo.dao.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/dge/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.getAllUsers(), new HttpHeaders(), HttpStatus.OK);
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.getUser(id).isPresent() ?
+                new ResponseEntity<>(userService.getUser(id).get(), new HttpHeaders(), HttpStatus.OK)
+                : new ResponseEntity<>( null, new HttpHeaders(), HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return new ResponseEntity<>(userService.saveUser(user), new HttpHeaders(), HttpStatus.CREATED);
+    }
+}
---
+++ src/main/java/com/example/demo/dao/UserService.java
@@ -0,0 +1,28 @@
+package com.example.demo.dao;
+
+import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public Optional<User> getUser(Long id){
+        return userRepository.findById(id);
+    }
+
+    public User saveUser(User user) {
+        return userRepository.save(user);
+    }
+}
